0959cce8a84eb733e9b2ef08c20b9cf80db59085
Nihal Chaurasia <nihal@delimp.com>
2023-04-25 16:27:57 +0530

    Body parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const http = require('http');
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 // Create an instance of the http server to handle HTTP requests
 
 
@@ -12,10 +12,20 @@ app.use((req, res, next) => {
         next();
     }
 });
-app.get('/webhooks', (req, res) => {
+app.get('https://webhooks-demo-production.up.railway.app/webhooks', (req, res) => {
     res.json({ title: "Nihal" });
 });
 
+app.post('https://webhooks-demo-production.up.railway.app/webhooks/tradingview', (req, res) => {
+    // Extract the data from the webhook payload
+    const data = req.body;
+
+    // Process the data as needed
+    console.log('Received webhook payload:', data);
+
+    // Send a response to TradingView
+    res.status(200).send('Webhook received successfully');
+});
 
 var port = process.env.PORT || 4000;
 
