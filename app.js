7ed9bb5fadeac5e9ebf7f4daaa1f88181d2e57c0
Nihal Chaurasia <nihal@delimp.com>
2023-04-25 16:13:09 +0530

    app .js changes

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const http = require('http');
-
 const express = require('express');
 const app = express();
+
 // Create an instance of the http server to handle HTTP requests
 
-
 app.get('/webhooks', (req, res) => {
-    res.json({ title: "Nihal" })
-})
+    res.json({ title: "Nihal" });
+});
+
 // Start the server on port 3000
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('Node server running on port 3000');
 });
 
+// Handle unhandled rejections and exceptions
+process.on('unhandledRejection', (err, promise) => {
+    console.error(`Unhandled rejection: ${err.message}`);
+    // Log the error to a file or a logging service
+});
 
+process.on('uncaughtException', (err, origin) => {
+    console.error(`Uncaught exception: ${err.message}`);
+    // Log the error to a file or a logging service
+    server.close(() => {
+        process.exit(1);
+    });
+});
 
